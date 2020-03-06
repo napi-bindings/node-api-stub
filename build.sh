e678aaf9e20d0376a75ca738cfb5316c64826f60
NickNaso <nicoladelgobbo@gmail.com>
2020-03-06 19:02:35 +0100

    Update build script and add build folder to gitignore.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 echo Start building process ...  && \
 
 echo Cleanup previous build ... && \
+rm -rf ./build && \
 rm -rf *.o && \
 rm -rf *.a && \
 echo Cleanup process completed. && \
@@ -16,6 +17,9 @@ fi
 gcc -c node_api.c -DNAPI_EXPERIMENTAL=1 && \
 ar -rcs libnode_api.a node_api.o && \
 ranlib libnode_api.a && \
+mkdir build && \
+mv node_api.o ./build/node_api.o && \
+mv libnode_api.a ./build/libnode_api.a && \
 echo N-API stub library successfully builded. && \
 
 echo Building process successfully ended.
